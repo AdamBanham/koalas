b5914ae469f8f0f0d2cf2d012493f934b5e3fbf0
Adam Peter Banham <n7176546@qut.edu.au>
2022-09-12 17:50:54 +1000

    added tests for simple exporter
    1 - it should be able to export sample logs
    2 - it should export then be read in to be equal to the original simple log

---
+++ tests/test_simple_export.py
@@ -0,0 +1,50 @@
+import unittest
+from os import path 
+from tempfile import TemporaryDirectory, gettempdir
+
+from koalas.read import read_xes_simple
+from koalas.export import export_to_xes_simple
+
+SSMALL = path.join(".","tests","small_01.xes")
+WSMALL = path.join(".","tests","small_02.xes")
+OSMALL = path.join(".","tests","small_03.xes")
+
+class DTLogTest(unittest.TestCase):
+
+    def test_successful_export(self):
+        try:
+            log = read_xes_simple(SSMALL, debug=False) 
+        except:
+            self.fail("Failed to parse log")
+
+        fdir = TemporaryDirectory()
+        try:
+            filepath = path.join(fdir.name, "test_log")
+            export_to_xes_simple(filepath, log,debug=False) 
+        except:
+            self.fail("Failed to export log")
+        finally:
+            fdir.cleanup()
+
+    def test_equals_export_import(self):
+        try:
+            log = read_xes_simple(SSMALL, debug=False) 
+        except:
+            self.fail("Failed to parse log")
+
+        fdir = TemporaryDirectory()
+        try:
+            filepath = path.join(fdir.name, "test_log")
+            export_to_xes_simple(filepath, log, debug=False) 
+        except:
+            fdir.cleanup()
+            self.fail("Failed to export log")
+            return
+        
+        log2 = read_xes_simple(filepath, sort=False,debug=False)
+        self.assertEqual(log, log2)
+
+        fdir.cleanup()
+
+
+
