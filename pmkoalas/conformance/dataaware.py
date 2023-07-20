08287d2b5382877348dc5123ffa12a146be78afb
Adam Peter Banham <n7176546@qut.edu.au>
2023-07-20 14:56:58 +1000

    guard-recall is now behaving [nonopt and opt]

--- pmkoalas/conformance/dataaware.py
+++ pmkoalas/conformance/dataaware.py
@@ -5,52 +5,64 @@ Implemented techniques include:
     - guard-recall 
     - guard-precision
 """
-
 from pmkoalas.complex import ComplexEventLog
-from pmkoalas.models.petrinet import LabelledPetriNet
+from pmkoalas.models.petrinet import PetriNetWithData
+from pmkoalas.models.guards import GuardOutcomes
 from pmkoalas.models.transitiontree import TransitionTreeGuardFlow
+from pmkoalas.models.transitiontree import TransitionTree
 from pmkoalas.conformance.matching import ManyMatching,EqualPathWeighter
-from pmkoalas.conformance.matching import construct_many_matching,ExpontentialPathWeighter
+from pmkoalas.conformance.matching import construct_many_matching, _computation_many_matching
+from pmkoalas.conformance.matching import ExpontentialPathWeighter
 from pmkoalas.models.transitiontree import construct_from_model
+from pmkoalas._logging import info, enable_logging, InfoQueueProcessor
+from pmkoalas._logging import InfoIteratorProcessor
+
+from joblib import Parallel, delayed
 
 def compute_directed_bookkeeping(flow:TransitionTreeGuardFlow, 
-    matcher:ManyMatching, log:ComplexEventLog, outcome=True) -> float:
+    matcher:ManyMatching, log:ComplexEventLog, outcome=GuardOutcomes.TRUE) \
+    -> float:
     """
     Computes the many directed bookkeeping on any flow for any given outcome.
     """
     bookkeeping = 0
     for trace, instances in log:
         weighter = ExpontentialPathWeighter(matcher[trace])
-        for instance in instances:
-            paths = matcher[trace]
-            for path in paths:
-                for step,i in zip(path, range(1, len(path)+1)):
-                    if step == flow: 
+        paths = matcher[trace]
+        for path in paths:
+            for step,i in zip(path, range(1, len(path)+1)):
+                if step == flow: 
+                    i_w = 0
+                    for instance in instances:
                         irveson = flow.guard().check(
                             instance.get_state_as_of(i-1)
                         ) == outcome
-                        bookkeeping += weighter(path, trace) * int(irveson)
+                        i_w += weighter(path, trace) if irveson else 0
+                    bookkeeping += i_w
+                    break
     return bookkeeping
 
 def compute_general_bookkeeping(flow:TransitionTreeGuardFlow, 
-    matcher:ManyMatching, log:ComplexEventLog, outcome=True) -> float:
+    matcher:ManyMatching, log:ComplexEventLog, outcome=GuardOutcomes.TRUE) \
+    -> float:
     """
     Computes the many general bookkeeping on any flow for any given outcome.
     """
     bookkeeping = 0
     for trace, instances in log:
         weighter = ExpontentialPathWeighter(matcher[trace])
-        for instance in instances:
-            paths = matcher[trace]
-            for path in paths:
-                for step,i in zip(path, range(1, len(path)+1)):
-                    if ( not isinstance(step, TransitionTreeGuardFlow)):
-                        continue
-                    if step.offering() == flow.offering(): 
+        paths = matcher[trace]
+        for path in paths:
+            for step,i in zip(path, range(1, len(path)+1)):
+                if step.offering() == flow.offering(): 
+                    i_w = 0
+                    for instance in instances:
                         irveson = flow.guard().check(
                             instance.get_state_as_of(i-1)
                         ) == outcome
-                        bookkeeping += weighter(path, trace) * int(irveson)
+                        i_w += weighter(path, trace) if irveson else 0
+                    bookkeeping += i_w
+                    break
     return bookkeeping
 
 def compute_total_weight(flow:TransitionTreeGuardFlow, 
@@ -60,59 +72,106 @@ def compute_total_weight(flow:TransitionTreeGuardFlow,
     """
     weight = 0.0
     for trace, instances in log:
-        weighter = ExpontentialPathWeighter(matcher[trace])
         paths = matcher[trace]
-        print(f"{trace=} with {len(paths)=}")
         for path in paths:
-            print(f"{str(path)=}")
             for step in path:
                 if step == flow: 
-                    weight += weighter(path, trace) * len(instances)
+                    weight += 1 * len(instances)
+                    break
     return weight
 
-def compute_guard_recall(log:ComplexEventLog, model:LabelledPetriNet) \
-    -> float: 
+def _computation_guard_recall(tree:TransitionTree, matching:ManyMatching, 
+    log:ComplexEventLog) -> float:
     """
-    Computes guard-recall of a data-aware process model, i.e. a Petri net with 
-    data.
+    The actual computation of guard-recall using the structures.
     """
-    # find longest observed trace
-    long = None
-    for trace,_ in log:
-        if long == None:
-            long = trace 
-        elif len(trace) > len(long):
-            long = trace 
-    long = len(long)
-    # prepare model
-    tree = construct_from_model(model, longest_playout=long)
-    matching = construct_many_matching(log, tree)
     flow_weight = set( 
         (flow, compute_total_weight(flow, matching, log))
         for flow
-        in tree.flows()
+        in InfoIteratorProcessor("computing total weight", tree.flows(), 
+                                 stack=4)
     )
-    flow_weight = set( 
-        (f,w)
-        for f,w 
-        in flow_weight
-        if w > 0.0
-    )
-    print(f"{len(flow_weight)=}")
-    recall = 1.0/ len(flow_weight)
-    print(f"{recall=}")
     inner_sum = 0.0
-    print(f"{inner_sum=}")
-    for flow,total_weight in flow_weight:
+    t_w = 0.0
+    for flow,total_weight in InfoIteratorProcessor("computing flow weight", 
+                                                   flow_weight):
         inner_sum += \
-            compute_directed_bookkeeping(flow, matching, log) / total_weight
-        print(f"{inner_sum=}")
-    return recall * inner_sum
+            compute_directed_bookkeeping(flow, matching, log)
+        t_w += total_weight
+    info(f"computed weights :: flow - {inner_sum:.2f} total - {t_w:.2f}")
+    return (inner_sum/t_w)
 
-def compute_guard_precision(log:ComplexEventLog, model:LabelledPetriNet) \
-    -> float:
+def _optimised_guard_recall(log:ComplexEventLog, tree:TransitionTree) -> float:
     """
-    Computes guard-precision of a data-aware process model, i.e a Petri net with
+    The computation of guard recall, whereby we only loop over the log once.
+    """
+    from pmkoalas.models.transitiontree import TransitionTreeGuardFlow
+    from pmkoalas.models.guards import GuardOutcomes
+    total_weight = 0
+    flow_weight = 0
+    # partial worker 
+    pool = Parallel(n_jobs=-2)
+    def partial(trace, instances, path, inst_w) -> float:
+        flow_weight = 0
+        total_weight = 0
+        for step,i in zip(path, range(1, len(path)+1)):
+                    if isinstance(step, TransitionTreeGuardFlow):
+                        for instance in instances:
+                            irveson = step.guard().check(
+                                    instance.get_state_as_of(i-1)
+                                ) == GuardOutcomes.TRUE
+                            flow_weight += inst_w(path, trace) * int(irveson)
+                        total_weight += 1 * len(instances)
+        return flow_weight, total_weight
+    # worker for inputs
+    def params(trace, instances, tree):
+        _,candidates = _computation_many_matching(trace,tree)
+        return trace, instances, candidates
+    # computation of work
+    inputs = list(pool( 
+        delayed(params)
+        (trace, instances, tree)
+        for trace,instances 
+        in log 
+    ))
+    inputs = [
+        (trace, instances, path, ExpontentialPathWeighter(matching))
+        for trace, instances, matching
+        in inputs 
+        for path 
+        in matching
+    ]
+    weights = pool( 
+        delayed(partial)
+        (traces, instances, path, inst_w)
+        for traces, instances, path, inst_w 
+        in InfoIteratorProcessor("processing variant-paths for guard-recall", 
+            inputs,
+            stack=8
+        )
+    )
+    info("completed processing.")
+    info(f"computed weights across flows (flow,total) :: {weights}")
+    flow_weight += sum(
+        f 
+        for f,t 
+        in weights
+    )
+    total_weight += sum( 
+        t 
+        for f,t 
+        in weights
+    )
+    recall = flow_weight / total_weight
+    info(f"computed guard recall of {recall:.3f}")
+    return recall
+
+@enable_logging
+def compute_guard_recall(log:ComplexEventLog, model:PetriNetWithData, 
+    optimised:bool=True) -> float: 
+    """
+    Quanitfies the quality between an event log and a data-aware process model 
+    using guard-recall, where a data-aware process model is a Petri net with 
     data.
     """
     # find longest observed trace
@@ -125,7 +184,19 @@ def compute_guard_precision(log:ComplexEventLog, model:LabelledPetriNet) \
     long = len(long)
     # prepare model
     tree = construct_from_model(model, longest_playout=long)
-    matching = construct_many_matching(log, tree)
+    if (optimised):
+        return _optimised_guard_recall(log, tree)
+    else:
+        info("creating matchings between all traces")
+        matching = construct_many_matching(log, tree)
+        # compute the measure 
+        return _computation_guard_recall(tree, matching, log)
+
+def _computation_guard_precision(tree:TransitionTree, matching:ManyMatching,
+    log:ComplexEventLog) -> float:
+    """
+    The actual computation of guard-precision using the structures.
+    """
     flow_weight = set( 
         (flow, compute_total_weight(flow,matching, log))
         for flow
@@ -144,3 +215,24 @@ def compute_guard_precision(log:ComplexEventLog, model:LabelledPetriNet) \
             compute_directed_bookkeeping(flow, matching, log) \
             / compute_general_bookkeeping(flow, matching, log)
     return prec * inner_sum 
+
+def compute_guard_precision(log:ComplexEventLog, model:PetriNetWithData) \
+    -> float:
+    """
+    Quanitfies the quality between an event log and a data-aware process model 
+    using guard-precision, where a data-aware process model is a Petri net with 
+    data.
+    """
+    # find longest observed trace
+    long = None
+    for trace,_ in log:
+        if long == None:
+            long = trace 
+        elif len(trace) > len(long):
+            long = trace 
+    long = len(long)
+    # prepare model
+    tree = construct_from_model(model, longest_playout=long)
+    matching = construct_many_matching(log, tree)
+    # compute measure
+    return _computation_guard_precision(tree, matching, log)
