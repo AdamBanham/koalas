24008e493c52c37c31f7f104f939adef90b42eb8
Adam Peter Banham <n7176546@qut.edu.au>
2023-10-11 14:28:55 +1000

    added template for testing grec and gprec

---
+++ tests/conformance/test_dataaware.py
@@ -0,0 +1,26 @@
+import unittest
+
+from os.path import join 
+
+from pmkoalas.conformance.dataaware import compute_guard_precision
+from pmkoalas.conformance.dataaware import compute_guard_recall
+
+class DataawareTests(unittest.TestCase):
+    
+    def test_computation_unopt(self):
+        pass 
+
+    def test_computation_opt(self):
+        pass 
+
+    def test_opt_recall_is_equalivent(self):
+        pass 
+
+    def test_opt_precision_is_equalivent(self):
+        pass 
+
+    def test_max_of_grec(self):
+        pass 
+
+    def test_max_of_gprec(self):
+        pass 
