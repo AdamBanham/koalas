d3065a2079c2a4f593252a7c1b20a40745f39460
Adam Peter Banham <n7176546@qut.edu.au>
2023-10-20 11:06:05 +1000

    updated checklist for release.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ import os
 ## be run, however to avoid these tests set the below to 'True'.
 ## tested affected by this setting have a decorator of 
 ## @unittest.skipIf(SKIP_SLOW, "testing can take up to 20s")
-os.environ['SKIP_SLOW_TESTS'] = 'False'
+os.environ['SKIP_SLOW_TESTS'] = 'True'
