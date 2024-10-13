5c510be630286bae24c6737d9facbbb09df536a6
Adam Peter Banham <n7176546@qut.edu.au>
2024-10-13 21:06:28 +1000

    added alpha-plus to discovery

--- pmkoalas/discovery/alpha_miner.py
+++ pmkoalas/discovery/alpha_miner.py
@@ -407,7 +407,7 @@ class AlphaMinerInstance(DiscoveryTechnique):
             places[place.name] = Place(place.name)
         for transition in TL:
             transitions[transition.name] = Transition(transition.name)
-        return LabelledPetriNet(
+        net = LabelledPetriNet(
             places.values(), transitions.values(),
             [Arc(
                 places[f.src.name] if 
@@ -418,6 +418,26 @@ class AlphaMinerInstance(DiscoveryTechnique):
                 else transitions[f.tar.name],
             )
             for f in FL])
+        imark = {}
+        fmark = {}
+        for place in net.places:
+            inc_arcs = set(
+                arc 
+                for arc in net.arcs 
+                if arc.to_node == place
+            )
+            out_arcs = set(
+                arc 
+                for arc in net.arcs 
+                if arc.from_node == place
+            )
+            if len(inc_arcs) == 0:
+                imark[place] = 1
+            if len(out_arcs) == 0:
+                fmark[place] = 1
+        net.set_initial_marking(imark)
+        net.set_final_marking(fmark)
+        return net
 
     def _step_one(self, log:EventLog) -> Set[str]:
         """
@@ -580,4 +600,308 @@ class AlphaMinerInstance(DiscoveryTechnique):
                     ))
         return flows
 
+### ------------------- Alpha Miner Plus ------------------- ###
+
+class AlphaPlusRelation(AlphaRelation):
+    """
+    A helper class to determine the ordering relations that captures
+    length-two loops.
+
+    Follows the work of the following article: Alves De Medeiros, A. K., 
+    Dongen, van, B. F., Aalst, van der, W. M. P., & Weijters, A. J. M. M. 
+    (2004). Process mining : extending the alpha-algorithm to mine short 
+    loops.
+
+    The ordering relations are described in Defintion 3.3. of the article.
+    """
+
+    def __init__(self, src: str, target: str, follows: List[Tuple[str]] = None) -> None:
+        super().__init__(src, target, follows)
+        self._two_step_follows = set()
     
+    def add_two_step_follows(self, src: str, follow: str) -> None:
+        if (src in self._AlphaRelation__members):
+            self._two_step_follows.add((src, follow))
+
+    def relation(self) -> AlphaRelationType:
+        # check for length two loop relations e.g. aba or bab 
+        # check for ab-? then check for aba 
+        loop_delta_a = False
+        if ((self.src,self.target) in self._follows):
+            if (self.target,self.src) in self._two_step_follows:
+                loop_delta_a = True
+        # check for ba-? then check for bab 
+        loop_delta_b = False
+        if ((self.target,self.src) in self._follows):
+            if (self.src,self.target) in self._two_step_follows:
+                loop_delta_b = True
+        
+        looping = loop_delta_b and loop_delta_a
+        # return one of the original alpha relations
+        if ((self.src,self.target) in self._follows and 
+            (self.target,self.src) in self._follows and
+            not looping
+        ):
+            return AlphaRelationType.PD 
+        elif ((self.src,self.target) in self._follows and 
+              (
+                    (self.target,self.src) not in self._follows
+                    or 
+                    looping
+             )
+        ):
+            return AlphaRelationType.CD 
+        elif ( (self.src,self.target) in self._follows):
+            return AlphaRelationType.DF
+        return AlphaRelationType.NF
+
+class AlphaMinerPlusInstance(DiscoveryTechnique):
+    """
+    Mines a Petri net using the alpha miner plus algorithm as presented in
+    Alves De Medeiros, A. K., 
+    Dongen, van, B. F., Aalst, van der, W. M. P., & Weijters, A. J. M. M. 
+    (2004). Process mining : extending the alpha-algorithm to mine short 
+    loops.
+
+    The alpha miner plus algorithm is an extension of the alpha miner,
+    whereby preprocessing the given log and adjusting the ordering relations
+    the alpha-plus miner can mine petri nets with short loops.
+    """
+
+    def __init__(self, min_inst:int=1 ,optimised:bool=False) -> None:
+        super().__init__()
+        self._opt = optimised
+        self._min_inst = min_inst
+
+    def mine_footprint_matrix(self, log:EventLog) -> Dict[
+        str,Dict[str,AlphaPlusRelation]]:
+        """
+        Mines the footprint matrix of the alpha miner relations
+        between process activities.
+        """
+        info("Mining footprint matrix...")
+        self._matrix = dict()
+        TL = self._step_one(log)
+        # set up matrix 
+        for src in TL:
+            self._matrix[src] = dict()
+            for target in TL:
+                self._matrix[src][target] = AlphaPlusRelation(
+                    src=src,target=target
+                )
+        # find directly flows lang
+        flang = log.directly_follow_relations()
+        # update relations
+        for col in self._matrix.values():
+            for relation in col.values():
+                src = relation.src
+                target = relation.target
+                # test flow relation from src to target
+                pair = DirectlyFollowPair(src, target, 1)
+                if (flang.contains(pair)):
+                    fpair = flang.find(pair)
+                    if fpair.frequency() >= self._min_inst:
+                        # print(f"adding follows ({src},{target}) to {relation}")
+                        relation.add_follows(src, target)
+                        # check for aba in proceeding
+                        if src in fpair.proceeding():
+                            relation.add_two_step_follows(src, target)
+                # test flow relation from target to src
+                pair = DirectlyFollowPair(target, src, 1)
+                if (flang.contains(pair)):
+                    fpair = flang.find(pair)
+                    if fpair.frequency() >= self._min_inst:
+                        # print(f"adding follows ({target},{src}) to {relation}")
+                        relation.add_follows(target, src)
+                        # check for bab in proceeding
+                        if target in fpair.proceeding():
+                            relation.add_two_step_follows(target, src)
+        # make new copy
+        out = dict()
+        for src in TL:
+            out[src] = deepcopy(self._matrix[src])
+        info("Footprint matrix mined.")
+        return deepcopy(self._matrix)
+    
+    def discover(self, log:EventLog) -> LabelledPetriNet:
+        """
+        Computes a Petri net to represent the given event log, assuming that
+        the event log is a loop-complete workflow log (def 3.1.).
+
+        The algorithm consists of nine steps where the alpha miner alogrithm
+        is called but mines ordering relations that capture length-two loops,
+        as described in Def. 4.4 of the article.
+        """
+        from pmkoalas.models.petrinet import Transition,Arc, Place
+        self._matrix = None
+        _ = self.mine_footprint_matrix(log)
+        #step one 
+        acts = self._step_one(log)
+        #step two 
+        doubles = self._step_two(log)
+        #step three 
+        nots = self._step_three(acts, doubles)
+        #step four 
+        fdoubles = self._step_four(doubles, nots)
+        #step five
+        wlog = self._step_five(log, doubles)
+        #step six
+        net = self._step_six(wlog)
+        # return net 
+        places = net.places 
+        transitions = net.transitions
+        ntransitions = set()
+        # the ids of the places need to match with the transitions
+        # for fun downstream stuff...
+        swaps = dict()
+        missing = set()
+        for f in fdoubles:
+            if isinstance(f.from_node,Place) and f.from_node.name not in swaps:
+                options = set( 
+                    p 
+                    for p in places 
+                    if p.name == f.from_node.name
+                )
+                if (len(options) > 0):
+                    swaps[f.from_node.name] = options.pop()
+                else:
+                    missing.add(f.from_node)
+            elif isinstance(f.from_node, Transition):
+                ntransitions.add(
+                    f.from_node
+                )
+            if isinstance(f.to_node,Place) and f.to_node not in swaps:
+                options = set( 
+                    p 
+                    for p in places 
+                    if p.name == f.to_node.name
+                )
+                if (len(options) > 0):
+                    swaps[f.to_node.name] = options.pop()
+                else:
+                    missing.add(f.to_node)
+            elif isinstance(f.to_node, Transition):
+                ntransitions.add(
+                    f.to_node
+                )
+        nfdoubles = set()
+        for f in fdoubles:
+            nfdoubles.add(
+                Arc(
+                    swaps[f.from_node.name] if f.from_node.name in swaps.keys() 
+                    else f.from_node,
+                    swaps[f.to_node.name] if f.to_node.name in swaps.keys()
+                    else f.to_node
+                )
+            )
+        # now we union the flows
+        transitions = transitions.union(ntransitions)
+        flows = net.arcs.union(nfdoubles)
+        places = places.union(missing)
+        net = LabelledPetriNet(
+            places, transitions, flows
+        )
+        imark = {}
+        fmark = {}
+        for place in net.places:
+            inc_arcs = set(
+                arc 
+                for arc in net.arcs 
+                if arc.to_node == place
+            )
+            out_arcs = set(
+                arc 
+                for arc in net.arcs 
+                if arc.from_node == place
+            )
+            if len(inc_arcs) == 0:
+                imark[place] = 1
+            if len(out_arcs) == 0:
+                fmark[place] = 1
+        net.set_initial_marking(imark)
+        net.set_final_marking(fmark)
+        return net 
+    
+    def _step_one(self, log:EventLog) -> Set[str]:
+        """
+        Returns a set of all process activities seen in the 
+        event log.
+        """
+        return log.seen_activities()
+    
+    def _step_two(self, log:EventLog) -> Set[str]:
+        """
+        Returns a set of process activities seen in the event, such that there
+        exists a trace in the log where ...aa... is seen.
+        """
+        doubles = set()
+        for trace in log.language():
+            for i in range(1,len(trace)):
+                if (trace[i-1] == trace[i]):
+                    doubles.add(trace[i])
+        return doubles
+    
+    def _step_three(self, acts: Set[str], doubles: Set[str]) -> Set[str]:
+        """
+        Simply returns the set of activities that are not in one-length loops.
+        """
+        return acts.difference(doubles)
+    
+    def _step_four(self, doubles:Set[str], nots: set[str]) -> Set['Arc']:
+        """
+        Returns a set of flow relations that capture the ordering relations
+        that capture length-one loops.
+        """
+        from pmkoalas.models.petrinet import Place,Transition,Arc
+        flows = set()
+        for t in doubles:
+            places_A = set(
+                a 
+                for a in nots 
+                if self._matrix[a][t].relation() == AlphaRelationType.CD
+            )
+            places_B = set(
+                b 
+                for b in nots 
+                if self._matrix[t][b].relation() == AlphaRelationType.CD
+            )
+            trans = Transition(t)
+            if len(places_A) == 0 and len(places_B) == 0:
+                place = AlphaStartPlace()
+            else:
+                pair = AlphaPair(places_A.difference(places_B), 
+                                places_B.difference(places_A)
+                )
+                place = AlphaPlace(str(pair), pair.left, pair.right)
+            place = Place(place.name)
+            flows.add(Arc(trans, place))
+            flows.add(Arc(place, trans))
+        return flows
+    
+    def _step_five(self, log:EventLog, doubles: set[str]) -> EventLog:
+        """
+        Returns a new event log where all traces have been processed to remove
+        activities that were identified in one-length loops.
+        """
+        from pmkoalas.simple import Trace
+        new_traces = []
+        for trace, freq in log.__iter__():
+            new_trace = []
+            for act in trace:
+                if act not in doubles:
+                    new_trace.append(act)
+            for i in range(freq):
+                new_traces.append(Trace(new_trace))
+        return EventLog(new_traces)
+    
+    def _step_six(self, log:EventLog) -> LabelledPetriNet:
+        """
+        Returns the Petri net discovered by the alpha miner using the alpha
+        plus relations.
+        """
+        miner = AlphaMinerInstance(min_inst=self._min_inst,optimised=self._opt)
+        miner.mine_footprint_matrix = self.mine_footprint_matrix
+        return miner.discover(log)
+        
+    
+
